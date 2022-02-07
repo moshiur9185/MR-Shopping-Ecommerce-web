f3f8f3aa709a74dfb8f19968822ab313cc823e24
moshiur9185 <moshiur9185@gmail.com>
2022-02-08 01:06:37 +0600

    work modify

--- src/Component/Feature/FeatureCard.js
+++ src/Component/Feature/FeatureCard.js
@@ -4,9 +4,8 @@ const FeatureCard = ({ info }) => {
   return (
     <div className="col-md-4 text-white info-cart text-center">
       <div className={`info-${info.background} info-container shadow`}>
-        <img className="pt-3" style={{ width: "4vw" }} src={info.icon} alt="" />
+        <img className="pt-3" style={{ width: "50px" }} src={info.icon} alt="" />
         <h5 className="pt-2">{info.title}</h5>
-        {/* <small>{info.description}</small> */}
       </div>
     </div>
   );
