80abff741d5d5e79bee65e512087bcea930ebda7
moshiur9185 <moshiur9185@gmail.com>
2022-02-07 12:17:31 +0600

    work done

--- src/Component/Navigation/Navigation.js
+++ src/Component/Navigation/Navigation.js
@@ -1,39 +1,21 @@
 import React from "react";
+import './Navigation.css';
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Navigation = () => {
   return (
     <div className="container-fluid">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">
-            Navbar
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-              <a class="nav-link" href="/">
-                Features
-              </a>
-              <a class="nav-link" href="/">
-                Pricing
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Navbar className="navigation" variant="dark">
+        <Container>
+          <Navbar.Brand className="logo" href="/home">XZ</Navbar.Brand>
+          <Nav className="ms-auto nav-link">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/features">Features</Nav.Link>
+            <Nav.Link href="/products">Products</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 };
--- src/Component/SecondProduct/SecondProduct.js
+++ src/Component/SecondProduct/SecondProduct.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import productImage from "../image/website-design.png";
+import productImage2 from "../image/coding (1).png";
+import productImage3 from "../image/graphic-design.png";
+import productImage4 from "../image/web-design.png";
+import productImage5 from "../image/images (1).jpg";
+import productImage6 from "../image/download (1).png";
+import productImage7 from "../image/download.png";
+import productImage8 from "../image/optimization.png";
 import ProductCard from '../ProductCard/ProductCard';
 import ProductThumbnail from "../image/images.jpg";
 
@@ -11,36 +18,36 @@ const SecondProduct = () => {
         },
         {
           title: "product 2",
-          image: productImage,
+          image: productImage2,
         },
         {
           title: "product 3",
-          image: productImage,
+          image: productImage3,
         },
         {
           title: "product 4",
-          image: productImage,
+          image: productImage4,
         },
         {
           title: "product 5",
-          image: productImage,
+          image: productImage5,
         },
         {
           title: "product 6",
-          image: productImage,
+          image: productImage6,
         },
         {
           title: "product 7",
-          image: productImage,
+          image: productImage7,
         },
         {
           title: "product 8",
-          image: productImage,
+          image: productImage8,
         },
       ];
     return (
         <section className="container text-center mt-5">
-        <div className="row">
+        <div className="row d-flex align-items-center">
           <div className="col-md-8">
             <div className="row">
             {products.map((product) => (
