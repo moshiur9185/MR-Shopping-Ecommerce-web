e4745962a1ee05d8b5b4f29024a8d1898a59f266
moshiur9185 <moshiur9185@gmail.com>
2022-02-07 12:47:06 +0600

    work modify

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import 'react-slideshow-image/dist/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Component/Home/Home";
+import Navigation from './Component/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navigation/>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
         </Routes>
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -2,14 +2,14 @@ import React from "react";
 import Feature from "../Feature/Feature";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-import Navigation from "../Navigation/Navigation";
+// import Navigation from "../Navigation/Navigation";
 import Product from "../Products/Products";
 import SecondProduct from "../SecondProduct/SecondProduct";
 
 const Home = () => {
   return (
     <div>
-      <Navigation />
+      {/* <Navigation /> */}
       <Header />
       <Feature />
       <div className="text-center mt-5">
