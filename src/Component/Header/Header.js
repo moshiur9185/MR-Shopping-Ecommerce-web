ee8cb12ff58daa1d816c99d187ddb3ab6f879fa3
moshiur9185 <moshiur9185@gmail.com>
2022-02-08 20:23:04 +0600

    work modify

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -7,7 +7,7 @@ import img3 from "../image/optimization.png";
 const Header = () => {
   return (
     <div className="text-white">
-      <Slide className="header-bg" easing="ease">
+      <Slide className="header-bg py-5" easing="ease">
         <div className="each-slide">
           <div className="row align-items-center">
             <div className="col-md-5 ms-md-5 ps-md-5 m-sm-2 text-md-start">
@@ -18,12 +18,12 @@ const Header = () => {
               </p>
               <button className="btn btn-primary">Get Start</button>
             </div>
-            <div className="col-md-6 mt-sm-5 text-center">
+            <div className="col-md-6   text-center">
               <img style={{ width: "32.42vh" }} src={img} alt="#" />
             </div>
           </div>
         </div>
-        
+
         <div className="each-slide">
           <div className="row align-items-center">
             <div className="col-md-5 ms-md-5 ps-md-5 m-sm-2 text-md-start">
@@ -34,7 +34,7 @@ const Header = () => {
               </p>
               <button className="btn btn-primary">Get Start</button>
             </div>
-            <div className="col-md-6 mt-sm-5 text-center">
+            <div className="col-md-6   text-center">
               <img style={{ width: "32.42vh" }} src={img3} alt="#" />
             </div>
           </div>
@@ -50,12 +50,11 @@ const Header = () => {
               </p>
               <button className="btn btn-primary">Get Start</button>
             </div>
-            <div className="col-md-6 mt-sm-5 text-center">
+            <div className="col-md-6   text-center">
               <img style={{ width: "32.42vh" }} src={img2} alt="#" />
             </div>
           </div>
         </div>
-
       </Slide>
     </div>
   );
