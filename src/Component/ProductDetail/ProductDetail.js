1db40b34c2bf91775e7e3325e63a49f6db32a75f
moshiur9185 <moshiur9185@gmail.com>
2022-03-04 01:57:07 +0600

    work update

--- src/Component/ProductDetail/ProductDetail.js
+++ src/Component/ProductDetail/ProductDetail.js
@@ -12,12 +12,15 @@ const ProductDetail = () => {
 
     const { title, image, price, detail } = electronics.find(product => product.id === id)
     return (
-        <div className="row">
-            <div className="col-md-6 pt-3 "><img  style={{ height: "70vh"}} src={image} alt="" /></div>
-            <div className="col-md-6 mt-5 ">
-                <h3>{title} </h3>
-                <h5>Price : {price} TK</h5>
-                <p>{detail}</p>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-6 pt-3 "><img style={{ height: "70vh" }} src={image} alt="" /></div>
+                <div className="col-md-5 mt-5 border border-secondary">
+                    <h3>Product : {title} </h3>
+                    <h5>Price : {price} TK</h5>
+                    <h6>Description :</h6>
+                    <p>{detail}</p>
+                </div>
             </div>
         </div>
     );
