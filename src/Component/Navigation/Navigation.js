b551e9149e155baa0c692426b459c89cbeb03106
moshiur9185 <moshiur9185@gmail.com>
2022-03-08 02:23:22 +0600

    work modify

--- src/Component/Navigation/Navigation.js
+++ src/Component/Navigation/Navigation.js
@@ -14,8 +14,8 @@ const Navigation = (props) => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-md-auto navbar">
-              <Nav.Link data-replace="Home" className="active nav" href="/"><span>Home</span></Nav.Link>
-              <Link data-replace="Products" className="ml-4 nav" to="/products"><span>Products</span></Link>
+              <Link data-replace="Home" className="active nav" to="/"><span>Home</span></Link>
+              <Link data-replace="Products" className="ml-4 nav " to="/products"><span>Products</span></Link>
               <Link data-replace="Blogs" className=" ml-4 nav" to="/blogs"><span>Blogs</span></Link>
               <Link data-replace="Contacts" className="ml-4 nav" to="/contact"><span>Contacts</span></Link>
               <Link data-replace="Contacts" className="ml-4 nav" to="/login"><span>Login</span></Link>
