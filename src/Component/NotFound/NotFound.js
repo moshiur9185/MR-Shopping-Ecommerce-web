b22b40fc97065547a38ab33315da483709d6184d
moshiur9185 <moshiur9185@gmail.com>
2022-03-10 00:23:24 +0600

    work update

--- src/Component/NotFound/NotFound.js
+++ src/Component/NotFound/NotFound.js
@@ -4,8 +4,8 @@ const NotFound = () => {
     return (
         <div className="h-screen bg-white text-center">
             <img className="h-5/6 w-screen" src="https://i.ibb.co/jGgqbYZ/istockphoto-1149316411-612x612.jpg" alt="" />
-            <h1>Page Not Found!</h1>
-            <p>The page you are looking for has not been found on our server.</p>
+            <h1 className="fw-bold">Page Not Found!</h1>
+            <p className="text-muted">The page you are looking for has not been found on our server.</p>
         </div>
     );
 };
