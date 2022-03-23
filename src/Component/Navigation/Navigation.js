d5cb93ca587bf5a60e6de886a8de131bdf6cd861
moshiur9185 <moshiur9185@gmail.com>
2022-03-23 12:55:05 +0600

    work modify

--- src/Component/Navigation/Navigation.js
+++ src/Component/Navigation/Navigation.js
@@ -11,10 +11,10 @@ const Navigation = (props) => {
 
   const { user, handleSignOut } = useAuth()
   return (
-    <div className="sticky-top">
-      <Navbar className="navigation " expand="lg">
-        <Container>
-          <Navbar.Brand className="logo" href="/">এম.আর. মার্ট</Navbar.Brand>
+    <div className="sticky-top ">
+      <Navbar className="navigation container-fluid" expand="lg">
+        <>
+          <Navbar.Brand className="logo" href="/">শপিং মার্ট</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-md-auto navbar">
@@ -22,11 +22,14 @@ const Navigation = (props) => {
               <Link data-replace="Products" className="ml-4 nav mt-2" to="/products"><span>Products</span></Link>
               <Link data-replace="Blogs" className=" ml-4 nav mt-2" to="/blogs"><span>Blogs</span></Link>
               <Link data-replace="Dashboard" className="ml-4 nav mt-2" to="/dashboard"><span>Dashboard</span></Link>
-              <Link data-replace="Cart" className="ml-4 nav mt-2" to="/cart"><span><FontAwesomeIcon icon={faShoppingCart} /> {props.cart.length}</span></Link>
-              {user.email ? <a href="/"><button className="btn btn-brand ml-4 mt-2" onClick={handleSignOut}>Sing Out</button></a> : <Link data-replace="Home" to="/login"><button className="btn btn-brand ml-4 mt-2">Login</button></Link> }
+              {user.email ? <a href="/"><button className="btn btn-brand ml-4 mt-2" onClick={handleSignOut}>Sing Out</button></a> : <Link data-replace="Home" to="/login"><button className="btn btn-brand ml-4 mt-2">Login</button></Link>}
             </Nav>
           </Navbar.Collapse>
-        </Container>
+        </>
+        <div className="pl-4 mt-2 lg:mx-0 md:mx-0 mx-auto">
+          <Link data-replace="Cart" className=" nav-cart " to="/cart"><span><FontAwesomeIcon icon={faShoppingCart} /> {props.cart.length}</span></Link>
+        </div>
+
       </Navbar>
     </div>
   );
